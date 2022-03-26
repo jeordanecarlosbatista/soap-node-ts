07d94a2cca160bff79e4f265ed4bd3c0548475fe
Jeordane Batista <jeordane.batista@hotmail.com>
2022-03-26 15:24:42 -0300

    create server soap

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,32 @@
 import express, { Express } from 'express';
+const soap = require('soap');
+const path = require('path');
+const fs = require('fs');
+
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
 const app: Express = express();
 
-app.use('/', (req, res) => {
-    res.json({ message: "Hello World" });
-})
+const myService = {
+    BookService: {
+        BookServiceSOAP: {
+            GetBook: function (args: any) {
+                return {
+                    ID: args.name,
+                    Title: 'Clean Code',
+                    Author: 'Robert Cecil Martin',
+                };
+            }
+        }
+    }
+}
 
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
+app.listen(port, () => {
+    const xml = fs.readFileSync(path.join(__dirname, '../book.wsdl'), 'utf8');
+    soap.listen(app, '/wsdl', myService, xml, function () {
+        console.log('server initialized');
+    });
+    console.log(`Server running at http://localhost:${port}`);
+})
